20e706e4234571e8a749fb213fcc8ed4bfb8ba8c
Rodrigo Gomes de Mendonça <rod_igomail@hotmail.com>
2021-09-20 16:41:03 -0300

    Resolvido o renderizarMascaraBombasAoRedor()

--- programa.js
+++ programa.js
@@ -1,8 +1,9 @@
 let Campo = {
     linhas: 5,
-    colunas: 6,
+    colunas: 8,
     bombas: 5,
     mascaraBombas: [],
+    mascaraBombasAoRedor: [],
     jogoFinalizado: false,
     renderizarHTML() {
         let corpo = document.querySelector("body");
@@ -13,7 +14,7 @@ let Campo = {
             html += "<tr>";
             for (let c = 1; c <= this.colunas; c++) {
                 html += `
-					<td id="l-${l}/c-${c}" class="I" onclick="mouseBotaoEsquerdo(${l}, ${c})" oncontextmenu="mouseBotaoDireito(${l}, ${c})">
+					<td id="l-${l}/c-${c}" class="I" onclick="Campo.mouseBotaoEsquerdo(${l}, ${c})" oncontextmenu="Campo.mouseBotaoDireito(${l}, ${c})">
 					</td>
 				`;
             }
@@ -27,7 +28,7 @@ let Campo = {
         let bombas = 0;
         let locaisBombas = [];
         let localBomba;
-        while (bombas <= this.bombas) {
+        while (bombas < this.bombas) {
             localBomba = [
                 Math.trunc(Math.random() * this.linhas) + 1,
                 Math.trunc(Math.random() * this.colunas) + 1,
@@ -51,7 +52,7 @@ let Campo = {
                 }
             }
         }
-        console.log(locaisBombas);
+        console.log("locaisBombas: ", locaisBombas);
         return locaisBombas;
     },
     renderizarMascaraBombas() {
@@ -62,6 +63,7 @@ let Campo = {
             }
             this.mascaraBombas.push(mascaraLinha);
         }
+        console.log("this.mascaraBombas: ", this.mascaraBombas);
     },
     colocarBombasNaMascaraBombas() {
         let locaisBombas = this.sortearLocaisBombas();
@@ -74,8 +76,82 @@ let Campo = {
                 }
             }
         }
-        console.log(locaisBombas);
-        console.log(this.mascaraBombas);
+        console.log("locaisBombas:", locaisBombas);
+        console.log("this.mascaraBombas: ", this.mascaraBombas);
+    },
+    mouseBotaoEsquerdo(l, c) {
+        console.log("pressisonado o botão esquerdo");
+    },
+    mouseBotaoDireito(l, c) {
+        document.querySelector(`[id="tabela"]`).oncontextmenu = (e) => {
+            e.preventDefault();
+        };
+        console.log("pressisonado o botão direito");
+    },
+    renderizarMascaraBombasAoRedor() {
+        let contarBombasAoRedor;
+        let mascaraBombasAoRedorLinha = [];
+        for (let l = 0; l < this.mascaraBombas.length; l++) {
+            for (let c = 0; c < this.mascaraBombas[l].length; c++) {
+                contarBombasAoRedor = 0;
+                if (this.mascaraBombas[l][c] == "B") {
+                    contarBombasAoRedor = "B";
+                } else {
+                    // acima
+                    try {
+                        if (this.mascaraBombas[l - 1][c] == "B") {
+                            contarBombasAoRedor++;
+                        }
+                    } catch (erro) {}
+                    // acima-direita
+                    try {
+                        if (this.mascaraBombas[l - 1][c + 1] == "B") {
+                            contarBombasAoRedor++;
+                        }
+                    } catch (erro) {}
+                    // direita
+                    try {
+                        if (this.mascaraBombas[l][c + 1] == "B") {
+                            contarBombasAoRedor++;
+                        }
+                    } catch (erro) {}
+                    // abaixo-direita
+                    try {
+                        if (this.mascaraBombas[l + 1][c + 1] == "B") {
+                            contarBombasAoRedor++;
+                        }
+                    } catch (erro) {}
+                    // abaixo
+                    try {
+                        if (this.mascaraBombas[l + 1][c] == "B") {
+                            contarBombasAoRedor++;
+                        }
+                    } catch (erro) {}
+                    // abaixo-esquerda
+                    try {
+                        if (this.mascaraBombas[l + 1][c - 1] == "B") {
+                            contarBombasAoRedor++;
+                        }
+                    } catch (erro) {}
+                    // esquerda
+                    try {
+                        if (this.mascaraBombas[l][c - 1] == "B") {
+                            contarBombasAoRedor++;
+                        }
+                    } catch (erro) {}
+                    // acima-esquerda
+                    try {
+                        if (this.mascaraBombas[l - 1][c - 1] == "B") {
+                            contarBombasAoRedor++;
+                        }
+                    } catch (erro) {}
+                }
+				mascaraBombasAoRedorLinha.push(contarBombasAoRedor);
+            }
+			this.mascaraBombasAoRedor.push(mascaraBombasAoRedorLinha);
+			mascaraBombasAoRedorLinha = [];
+        }
+        console.log(this.mascaraBombasAoRedor);
     },
 };
 
@@ -83,3 +159,4 @@ Campo.renderizarHTML();
 
 Campo.renderizarMascaraBombas();
 Campo.colocarBombasNaMascaraBombas();
+Campo.renderizarMascaraBombasAoRedor();
